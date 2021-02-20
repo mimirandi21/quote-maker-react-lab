661ca60c9f8434e6be125646ee3644358133346a
Randi Schreiner <mimirandi21@gmail.com>
2021-02-20 15:52:51 -0600

    commit tree 6c7d63e8ec5ea7542e158811f05783ad07a85052

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,66 +1,84 @@
-import React, { Component } from 'react';
-import uuid from 'uuid';
-import { connect } from 'react-redux';
-import { addQuote } from '../actions/quotes';
+import React, { Component } from "react";
+import uuid from "uuid";
+import { connect } from "react-redux";
+import { addQuote } from "../actions/quotes";
 
 class QuoteForm extends Component {
+	state = {
+		content: "",
+		author: "",
+	};
 
-  state = {
-    //set up a controlled form with internal state
-  }
+	handleOnChange = (event) => {
+		const { value, name } = event.target;
+		this.setState({
+			[name]: value,
+		});
+	};
 
-  handleOnChange = event => {
-    // Handle Updating Component State
-  }
+	handleOnSubmit = (event) => {
+		event.preventDefault();
+		let quote = { ...this.state, id: uuid() };
+		this.props.addQuote(quote);
+		this.setState({
+			content: "",
+			author: "",
+		});
+	};
 
-  handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-md-offset-2">
-            <div className="panel panel-default">
-              <div className="panel-body">
-                <form className="form-horizontal">
-                  <div className="form-group">
-                    <label htmlFor="content" className="col-md-4 control-label">Quote</label>
-                    <div className="col-md-5">
-                      <textarea
-                        className="form-control"
-                        value={this.state.content}
-                      />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="author" className="col-md-4 control-label">Author</label>
-                    <div className="col-md-5">
-                      <input
-                        className="form-control"
-                        type="text"
-                        value={this.state.author}
-                      />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-8 col-md-offset-2">
+						<div className="panel panel-default">
+							<div className="panel-body">
+								<form
+									className="form-horizontal"
+									onSubmit={this.handleOnSubmit}
+								>
+									<div className="form-group">
+										<label htmlFor="content" className="col-md-4 control-label">
+											Quote
+										</label>
+										<div className="col-md-5">
+											<textarea
+												className="form-control"
+												name="content"
+												value={this.state.content}
+												onChange={this.handleOnChange}
+											/>
+										</div>
+									</div>
+									<div className="form-group">
+										<label htmlFor="author" className="col-md-4 control-label">
+											Author
+										</label>
+										<div className="col-md-5">
+											<input
+												className="form-control"
+												name="author"
+												type="text"
+												value={this.state.author}
+												onChange={this.handleOnChange}
+											/>
+										</div>
+									</div>
+									<div className="form-group">
+										<div className="col-md-6 col-md-offset-4">
+											<button type="submit" className="btn btn-default">
+												Add
+											</button>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, { addQuote })(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,33 @@
-export default (state = [], action) => {
-  return state;
+export default function (state = [], action) {
+	let quote;
+	switch (action.type) {
+		case "ADD_QUOTE":
+			return state.concat(action.quote);
+		case "REMOVE_QUOTE":
+			let thisGuy = state.findIndex((quote) => quote.id === action.quoteId);
+			return [...state.slice(0, thisGuy), ...state.slice(thisGuy + 1)];
+		case "UPVOTE_QUOTE":
+			let thisOne = state.findIndex((quote) => quote.id === action.quoteId);
+			quote = state[thisOne];
+			return [
+				...state.slice(0, thisOne),
+				Object.assign({}, quote, { votes: quote.votes + 1 }),
+				...state.slice(thisOne + 1),
+			];
+
+		case "DOWNVOTE_QUOTE":
+			let thatOne = state.findIndex((quote) => quote.id === action.quoteId);
+			quote = state[thatOne];
+			if (quote.votes > 0) {
+				return [
+					...state.slice(0, thatOne),
+					Object.assign({}, quote, { votes: quote.votes - 1 }),
+					...state.slice(thatOne + 1),
+				];
+			} else {
+				return state;
+			}
+		default:
+			return state;
+	}
 }
